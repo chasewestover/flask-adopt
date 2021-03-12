db83af2701a87318ef439e15651941db0070598f
Chase Westover <chasejwestover@gmail.com>
2021-03-12 13:30:43 -0600

    added Spots

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """Flask app for adopt app."""
 
-from flask import Flask
+from flask import Flask, render_template
+from models import Pet
 
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -14,7 +15,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///adopt"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 connect_db(app)
-db.create_all()
+
 
 # Having the Debug Toolbar show redirects explicitly is often useful;
 # however, if you want to turn it off, you can uncomment this line:
@@ -22,3 +23,17 @@ db.create_all()
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
+
+
+@app.route("/")
+def show_homepage():
+
+    db.drop_all()
+    db.create_all()
+
+    p1 = Pet(name="Spots",species="dog", age='young', photo_url='https://tse3.mm.bing.net/th?id=OIP.qCtOqxrCFWQmBnwwEkJyxQHaHa&pid=Api')
+    db.session.add(p1)
+    db.session.commit()
+
+    pets = Pet.query.all()
+    return render_template("homepage.html", pets=pets)
--- models.py
+++ models.py
@@ -13,3 +13,16 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+class Pet(db.Model):
+
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20), nullable=False)
+    species = db.Column(db.String(20), nullable=False)
+    photo_url = db.Column(db.Text, default='')
+    age = db.Column(db.String(20), nullable=False)
+    notes = db.Column(db.Text)
+    available = db.Column(db.Boolean, default=True)
+
